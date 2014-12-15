496348b7b3c09506d15f5f9b020c134e5563a607
Dmitriy Okhotnikov <me3sa@me.com>
2014-12-15 17:09:05 +0300

    init

---
+++ Rutor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rutor
+//
+//  Created by Dmitriy Okhotnikov on 15.12.14.
+//  Copyright (c) 2014 Dmitriy Okhotnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
